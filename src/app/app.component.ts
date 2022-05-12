208ad84a5dd0730bd8723b171f13940921a4c729
Nithiya03 <nithiyasrirud03@gmail.com>
2022-05-12 17:59:59 +0530

    angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'structural';
+  title = 'structural-directives';
+  ifcase=false;
+  gender="male";
+  lottery="yes";
+  color="green";
+  status="ok";
+  countries=['Japan','China','Korea','India','France','Italy']
+  phoneCode=[
+    {
+      country:'Japan',
+      code:81
+    },
+    {
+      country:'China',
+      code:86
+    },
+    {
+      country:'Korea',
+      code:82
+    },
+    {
+      country:'India',
+      code:91
+    },
+    {
+      country:'France',
+      code:33
+    },
+    {
+      country:'Italy',
+      code:39
+    }
+  ]
 }
